ce0d330c4b8eab04e5d7e4a930de7d2033e0913c
Sharyar <kaka929230@gmail.com>
2025-09-06 18:05:26 +0500

    import_csv.py

--- import_csv.py
+++ import_csv.py
@@ -3,10 +3,9 @@ import sqlite3
 
 csv_file = "users.csv"
 
-# CSV faylni o‘qish
 df = pd.read_csv(csv_file)
 
-# SQLite bazasi
+# Bazaga import qilish
 conn = sqlite3.connect("users.db")
 df.to_sql("users", conn, if_exists="replace", index=False)
 conn.close()
