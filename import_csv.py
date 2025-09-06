74216c3c70c85bc40d50e5c4e58fa5b9b33668c3
Sharyar <kaka929230@gmail.com>
2025-09-06 16:55:11 +0500

    import_csv.py

--- import_csv.py
+++ import_csv.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import sqlite3
 
-# CSV faylni o‘qish
-df = pd.read_csv("users.csv")
+csv_file = "users.csv"
 
-# SQLite bilan bog‘lanish
+# CSV faylni o‘qish
+df = pd.read_csv(csv_file)
+
+# SQLite bazasi
 conn = sqlite3.connect("users.db")
 df.to_sql("users", conn, if_exists="replace", index=False)
 conn.close()
+
 print("✅ CSV ma’lumotlari bazaga import qilindi")
